16bf9990df2c6f19277ee314068effa41b37e534
Julian Müller <julian2.mueller@tum.de>
2021-09-01 17:11:56 +0200

    Clean Up Code and avoid repeated axios calls (by eliminating some of the watchers)

--- SensitivityBarPlot.vue
+++ SensitivityBarPlot.vue
@@ -12,7 +12,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 import barplot from '@/vue-d3-components/GenericBarPlot'
 
 export default {
@@ -29,85 +28,30 @@ export default {
       type: Number,
       default: 200
     },
-    title: {
-      type: String,
-      default: ''
-    },
-    selectedDataset: {
-      type: Object,
-      default: null
-    },
-    selectedDrugs: {
-      type: String,
-      default: ''
-    },
-    selectedTissues: {
-      type: String,
-      default: ''
-    },
     selectedKey: {
       type: String,
       default: ''
     },
-    selectedLinesForBarPlot: {
-      type: String,
-      default: ''
-    }
-  },
-  data: function () {
-    return {
-      barPlotModel: null
+    violinModel: {
+      type: Array,
+      default: () => []
     }
   },
   computed: {
+    barPlotModel: function(){
+      if(this.selectedKey){
+      return this.violinModel.filter(model => model.AttributeType === this.selectedKey)[0];
+      }
+      else{
+        return null;
+      }
+    },
+    
     barPlotTitle: function () {
       return this.selectedKey + "  values for selected drug/cell line combination"
     }
   },
-  watch: {
-    selectedDataset: function () {
-      this.getData()
-    },
-    selectedDrugs: function () {
-      this.getData()
-    },
-    selectedTissues: function () {
-      this.getData()
-    },
-    selectedKey: function () {
-      this.getData()
-    },
-    selectedLinesForBarPlot: function () {
-      this.getData()
-    }
-  },
   methods: {
-    getData: function () {  
-      if (this.selectedKey) {
-        let that = this
-        
-        let urlDatasets = this.$store.state.host+'/proteomicsdb/logic/cellSelectivity/getDataForVioPlots.xsjs'
-        axios.get(urlDatasets, {
-          params: {
-            dataset_id: that.selectedDataset.datasetId,
-            drug_id: that.selectedDrugs,
-            cell_line_id: that.selectedTissues,
-            BIC: 100,
-            R2: 0,
-            Effect: 0,
-            Switch: 1,
-            model_ids: that.selectedLinesForBarPlot
-          }})
-          .then(function (response) {
-            var selectedKeyBarData = response.data.filter(function(ViolinModel) {
-                return ViolinModel.AttributeType === that.selectedKey;
-              })[0];
-            that.barPlotModel = selectedKeyBarData
-          })
-      } else {
-        this.barPlotModel = null
-      }
-    },
     onBarSelected: function (obj) {
       this.$emit('send-message', obj)
     },
--- SensitivityParallelPlot.vue
+++ SensitivityParallelPlot.vue
@@ -1,15 +1,14 @@
 <template>
-  <parallelplot 
-     v-if="violinModel"
-     :chartData="violinModel" 
-     :plotWidth="minWidth" 
-     :plotHeight="minHeight" 
-     @keyChange="keyChange"
-     />
+  <parallelplot
+    v-if="violinModel"
+    :chartData="violinModel"
+    :plotWidth="minWidth"
+    :plotHeight="minHeight"
+    @keyChange="keyChange"
+  />
 </template>
 
 <script>
-import axios from 'axios'
 import parallelplot from '@/vue-d3-components/D3ParallelGeneric'
 
 export default {
@@ -26,61 +25,12 @@ export default {
       type: Number,
       default: 200
     },
-    selectedDataset: {
-      type: Object,
-      default: null
-    },
-    selectedDrugs: {
-      type: String,
-      default: ''
-    },
-    selectedTissues: {
-      type: String,
-      default: ''
-    }
-  },
-  data: function () {
-    return {
-      violinModel: null
-    }
-  },
-  watch: {
-    selectedDataset: function () {
-      this.getData()
-    },
-    selectedDrugs: function () {
-      this.getData()
-    },
-    selectedTissues: function () {
-      this.getData()
+    violinModel: {
+      type: Array,
+      default: () => []
     }
   },
   methods: {
-    getData: function () {
-      if (this.selectedDrugs && this.selectedTissues && !(this.selectedDrugs === '-1' && this.selectedTissues === 'not')) {
-        let that = this
-        let urlDatasets = this.$store.state.host+'/proteomicsdb/logic/cellSelectivity/getDataForVioPlots.xsjs'
-
-        axios.get(urlDatasets, {
-          params: {
-            dataset_id: that.selectedDataset.datasetId,
-            drug_id: that.selectedDrugs,
-            cell_line_id: that.selectedTissues,
-            BIC: 100,
-            R2: 0,
-            Effect: 0,
-            Switch: 1
-          }})
-          .then(function (response) {
-            that.violinModel = response.data
-            that.$emit('modelUpdate', that.violinModel)
-          })
-      } else {
-        this.violinModel = null
-        this.$emit('modelUpdate', null)
-        this.$emit('keyChange', null, null)
-      }
-    },
     keyChange: function (selectedLinesForBarPlot, selectedKey) {
       this.$emit('keyChange', selectedLinesForBarPlot, selectedKey)
     },
